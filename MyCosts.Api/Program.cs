5f737a8558e1576584ed6159c84ec95dcedc5f76
Oleg Esikov <esikov.oleg@mail.ru>
2024-01-21 19:53:39 +0300

    refactor: swagger gen settings have encapsulated to SwaggerGenBuilder class

--- MyCosts.Api/Program.cs
+++ MyCosts.Api/Program.cs
@@ -1,11 +1,9 @@
-using System.Reflection;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using MyCosts.Api.Extensions;
 using MyCosts.Api.Models.Config;
-using MyCosts.Api.Swagger.OperationFilters;
+using MyCosts.Api.Swagger;
 using MyCosts.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,42 +15,7 @@ builder.Services
     .ConfigureApiBehaviorOptions(options => { options.SuppressMapClientErrors = true; });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "MyCosts API",
-        Version = "v1",
-        Description = "MyCosts API documentation",
-        Contact = new OpenApiContact
-        {
-            Name = "Oleg Esikov",
-            Email = "esikov.oleg@mail.ru",
-            Url = new Uri("https://github.com/esiole"),
-        },
-        License = new OpenApiLicense
-        {
-            Name = "MIT License",
-            Url = new Uri("https://opensource.org/licenses/MIT"),
-        },
-    });
-
-    var xmlDocFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlDocFileName));
-
-    options.SupportNonNullableReferenceTypes();
-
-    options.AddSecurityDefinition(SecureEndpointAuthRequirementFilter.SchemeName, new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. Enter your token in the text input below",
-    });
-
-    options.OperationFilter<SecureEndpointAuthRequirementFilter>();
-});
+builder.Services.AddSwaggerGen(SwaggerGenBuilder.SetupSwaggerGen);
 
 builder.Services
     .AddSingleton(jwtOptions)
---
+++ MyCosts.Api/Swagger/SwaggerGenBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using Microsoft.OpenApi.Models;
+using MyCosts.Api.Swagger.OperationFilters;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MyCosts.Api.Swagger;
+
+public static class SwaggerGenBuilder
+{
+    public static void SetupSwaggerGen(SwaggerGenOptions options)
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "MyCosts API",
+            Version = "v1",
+            Description = "MyCosts API documentation",
+            Contact = new OpenApiContact
+            {
+                Name = "Oleg Esikov",
+                Email = "esikov.oleg@mail.ru",
+                Url = new Uri("https://github.com/esiole"),
+            },
+            License = new OpenApiLicense
+            {
+                Name = "MIT License",
+                Url = new Uri("https://opensource.org/licenses/MIT"),
+            },
+        });
+
+        var xmlDocFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlDocFileName));
+
+        options.SupportNonNullableReferenceTypes();
+
+        options.AddSecurityDefinition(SecureEndpointAuthRequirementFilter.SchemeName, new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. Enter your token in the text input below",
+        });
+
+        options.OperationFilter<SecureEndpointAuthRequirementFilter>();
+    }
+}
